9f3f5f743d7be499e138710359ae4eeb2423a6de
wangchao <914111374>
2018-08-09 19:34:46 +0800

    updated

--- service/ruleout.go
+++ service/ruleout.go
@@ -5,5 +5,5 @@ import "github.com/globalsign/mgo/bson"
 // 排除的分类
 var RuleOut = []bson.ObjectId {
 	bson.ObjectIdHex("5b6bd55a50456c5fb99610f5"), // 伦理片
-	//bson.ObjectIdHex("5b0fd14e7cad175a34a2ea8c"): true,
+	bson.ObjectIdHex("5b6c1f84adcfce70593225a9"), // 福利片
 }
