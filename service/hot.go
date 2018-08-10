a7123ecabc7c431ccd39346faf7f7185c4cd80ee
wangchao <914111374>
2018-08-10 08:53:51 +0800

    updated

--- service/hot.go
+++ service/hot.go
@@ -13,7 +13,7 @@ func GetHotsThumbnail (list []bson.M) {
 		// 并发的关联
 		go func(v bson.M) {
 			defer wg.Done()
-			v["video"], _ = video.M.FindById(v["vid"], "thumbnail")
+			v["video"], _ = video.M.FindById(v["vid"], "thumbnail, source")
 		}(v)
 	}
 	wg.Wait()
